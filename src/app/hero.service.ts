bf816308682ea1d4497fa589926e9eb82c1b2a45
sophie <sophiecode1105@gmail.com>
2022-06-20 18:55:20 +0900

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
+
+  private heroesUrl = 'api/heroes';
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // 지금은 히어로의 `id` 프로퍼티가 항상 존재한다고 간주합니다.
+    // 에러를 처리하는 방법은 다음 튜토리얼에 대해 알아봅니다.
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+}
